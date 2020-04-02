a595119559db55848f5d718ea2d71d593d95458d
Vishakha2301 <62894099+Vishakha2301@users.noreply.github.com>
2020-04-02 19:34:01 +0530

    Adding src/home and src/shirt

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 import './Home.css';
-import men_shirt from './mens-shirt_78211.png';
-import men_pant from './men-pant_78206.png';
-import ImageBndl from './ImageBndl.js'
+import { Button } from 'react-bootstrap';
+import men_shirt from './mens-shirt_78211.svg';
+import men_pant from './men-pant_78206.svg';
 class Home extends Component {
   render() {
-    
+
     return (
-      <div>
-        <ImageBndl
-        url = {men_shirt}
-        />
-        <button >Shirt</button>
-        <ImageBndl
-        url =  {men_pant}
-        />
-        <button>Pant</button>
-      
+      <div class="row text-center">
+        <div class="col-12">
+          <img class="img-thumbnail logo"
+            src={men_shirt}
+          />
+          <div class="col-12">
+            <Button variant="secondary">Shirt</Button>
+          </div>
+        </div>
+        <div class="col-12">
+          <img class="img-thumbnail logo"
+            src={men_pant}
+          />
+          <div class="col-12">
+            <Button variant="secondary">Pant</Button>
+          </div>
+        </div>
       </div>
-    
+
     )
   }
 }
---
+++ src/shirt/shirt.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './shirt.css';
+
+class shirt extends Component {
+  // constructor(props){
+    // super(props);
+    // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return (
+      <div>Hi Shirt</div>
+    );
+  }
+}
+
+export default shirt;
