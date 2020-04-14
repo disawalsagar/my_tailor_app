e7c40ac43dfbc9e13e71e471d1e70c187d99fc8c
Disawal <sdisawal@deloitte.com>
2020-04-14 01:04:40 -0500

    Addition to PDF module

--- src/shirt/GenPDF.js
+++ src/shirt/GenPDF.js
@@ -4,29 +4,45 @@ import {
     Text,
     View,
     Document,
-    StyleSheet,
-    Image
+    StyleSheet
 } from "@react-pdf/renderer";
+import Header from './Header';
 
 const styles = StyleSheet.create({
     page: {
-        backgroundColor: "#ffffff"
+        backgroundColor: "#ffffff" ,
+        padding: 30,
+          
     },
-    section: { color: 'black', textAlign: 'center'}
+    section: { color: 'black', textAlign: 'center' },
+    header: {
+        flexDirection: 'row',
+        borderBottomWidth: 2,
+        borderBottomColor: '#112131',
+        borderBottomStyle: 'solid',
+        alignItems: 'stretch',
+       
+      },
 });
+
+
+
+
+
+
 function GenPDF(props) {
 
-   
-   
-        console.log("pdf props", props.data);
-        return (
-            <Document>
-                <Page size="A4" style={styles.page} >
-                <View style={styles.section}>
-                                <Text >Measurement</Text>
-                              
-                                
-                 </View>
+
+
+    console.log("pdf props", props.data);
+
+    return (
+        <Document>
+            <Page size="Letter" style={styles.page} >
+                
+            <Header />
+                
+
                 {props.data
                     ? Object.entries(props.data).map(([key, value]) => {
                         return (
@@ -38,8 +54,8 @@ function GenPDF(props) {
                     : ""}
             </Page>
         </Document>
-        );
-    
+    );
+
 }
 
 
